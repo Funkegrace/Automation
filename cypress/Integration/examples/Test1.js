c25d5034fb738873ffa4a560b3370e9f9fdc1a19
Funkegrace <olanrewajufunke28@gmail.com>
2025-09-15 12:22:10 +0200

    initial commit

---
+++ cypress/Integration/examples/Test1.js
@@ -0,0 +1,10 @@
+///<reference types="Cypress"/>
+describe ('first test suite', function ()
+{
+    it ('first test case', function () {
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+    cy.
+    })
+}
+
+)
