ca336735030361633e38510be31061092c4a906e
Funkegrace <olanrewajufunke28@gmail.com>
2025-09-19 10:12:21 +0200

    parent child chaining

--- cypress/Integration/examples/Test1.js
+++ cypress/Integration/examples/Test1.js
@@ -3,7 +3,14 @@ describe ('first test suite', function ()
 {
     it ('first test case', function () {
         cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
-    cy.
+        cy.get('.search-keyword').type('ca');
+        cy.wait(2000)
+        cy.get('.product:visible').should('have.length',4)
+        //parent-child chaining
+        cy.get('.products').find('.product').should('have.length',4)
+
+        cy.get('.products').find('.product').eq(2).contains('ADD TO CART')
+    
     })
 }
 
