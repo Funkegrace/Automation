06ac4c039529b4f126383eecdfab8b3bef5d4d76
Funkegrace <olanrewajufunke28@gmail.com>
2025-09-20 23:16:44 +0200

    using the each method to loop through an array

--- cypress/Integration/examples/Test1.js
+++ cypress/Integration/examples/Test1.js
@@ -9,7 +9,14 @@ describe ('first test suite', function ()
         //parent-child chaining
         cy.get('.products').find('.product').should('have.length',4)
 
-        cy.get('.products').find('.product').eq(2).contains('ADD TO CART')
+        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+        
+        cy.get('.products').find('.product').each($el, indexedDB, $list) => {
+
+            const textVeg = $el.find('h4.product-name').text()
+            if(textVeg.include('cashews')) 
+                {$el.find('button')}
+        }
     
     })
 }
