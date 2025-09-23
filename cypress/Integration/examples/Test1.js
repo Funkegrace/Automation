c974fd0b8fc79226db23a30cdf5863217baf4fea
Funkegrace <olanrewajufunke28@gmail.com>
2025-09-23 21:16:21 +0200

    update comment

--- cypress/Integration/examples/Test1.js
+++ cypress/Integration/examples/Test1.js
@@ -6,7 +6,7 @@ describe ('first test suite', function ()
         cy.get('.search-keyword').type('ca');
         cy.wait(2000)
         cy.get('.product:visible').should('have.length',4)
-        //parent-child chaining
+        //parent-child chaining for cy test
         cy.get('.products').find('.product').should('have.length',4)
 
         cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
