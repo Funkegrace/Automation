942e2bfee5c7829576ed06a67840226d0cccb0da
Funkegrace <olanrewajufunke28@gmail.com>
2025-10-10 19:26:31 +0200

    code for select dropdown

--- cypress/Integration/examples/Test3.js
+++ cypress/Integration/examples/Test3.js
@@ -5,11 +5,22 @@ describe('My Third Test Suite', function()
  
 it('My Third Test case',function() {
  
- 
+ //checkboxes
 cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
 cy.get('#checkBoxOption1').check().should('be.checked').and('have.value', 'option1')
 cy.get('#checkBoxOption1').uncheck().should('not.be.checked')
 cy.get('input[type="checkbox"]').check(['option2','option3'])
+
+//select
+cy.get('select').select('option3').should('have.value', 'option3') //selecting static dropdown
+
+cy.get('#autocomplete').type('ind')
+cy.get('.ui-menu-item div').each(($el,index,$list) =>{
+if($el.text() === 'India')
+    {
+        $el.click()
+    }
+})
 
 })
 })
